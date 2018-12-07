c44a207029040700ff4f792d3e7eab5c180d29b2
not2sy <jj@wivlabs.com>
2018-12-07 15:43:57 +0900

    /api/bookings2 구현

--- gcrawler/utils/date.py
+++ gcrawler/utils/date.py
@@ -16,6 +16,21 @@ def convert_str_to_datetime(str, format='%Y.%m.%d %H:%M'):
     return datetime.strptime(str, format)
 
 
+WEEKDAY = {
+    0: 'MON',
+    1: 'TUE',
+    2: 'WED',
+    3: 'THU',
+    4: 'FRI',
+    5: 'SAT',
+    6: 'SUN',
+}
+def convert_str_to_weekday(str, format='%Y%m%d'):
+    global WEEKDAY
+    return WEEKDAY[convert_str_to_datetime(str, format).weekday()]
+
+
 if __name__ == '__main__':
     # print(date_str_range(14))
-    print(convert_str_to_datetime('2018.11.26 07:59'))
+    # print(convert_str_to_datetime('2018.11.26 07:59'))
+    convert_str_to_weekday('20181207')
--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,9 @@
+import re
+from json import loads
+from pprint import pprint
+from ddict import DotAccessDict
+from collections import defaultdict
+from gcrawler.utils.date import convert_str_to_weekday
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
@@ -36,16 +42,82 @@ class BookingList(generics.ListCreateAPIView, generics.RetrieveDestroyAPIView):
     serializer_class = BookingSerializer
     lookup_field = 'golf_course'
 
-from ddict import DotAccessDict
 
+R_NONDIGIT = re.compile('[^\d]+')
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
 def bookings2(request):
-    print(request.query_params.keys())
+    query = DotAccessDict(loads(request.query_params.get('params', '{}')))
 
-    for _key in request.query_params.keys():
-        print('_key:', _key)
+    def parse_date():
+        return query.booking_dates \
+               and {'kickoff_date__in': list(map(lambda x: R_NONDIGIT.subn('', x)[0], query.booking_dates))} \
+               or {}
 
-    query = DotAccessDict(request.query_params)
+    def parse_time():
+        str_zfill = lambda x: str(x).zfill(2)
+        return query.time_range \
+               and {'kickoff_hour__gte': str_zfill(query.time_range['from']), 'kickoff_hour__lte': str_zfill(query.time_range['to'])} \
+               or {}
+
+    def parse_region():
+        return query.region \
+               and {'golf_course__region': query.region} \
+               or {}
+
+    def parse_course():
+        return query.course \
+               and {'golf_course__id': query.course} \
+               or {}
+
+    def parse_price():
+        return query.greenfee_range \
+               and {'price__gte': int(query.greenfee_range['from']), 'price__lte': int(query.greenfee_range['to'])} \
+               or {}
+
+    def get_kickoffs(dates, bookings):
+        items = defaultdict(list)
+        for _item in bookings:
+            items[_item.kickoff_date].append({
+                'id': _item.site.id,
+                'name': _item.site.name,
+                'icon_url': _item.site.icon_url,
+                'kickoff_time': _item.kickoff_time.strftime('%Y.%m.%d %H:%M'),
+            })
+
+        kickoffs = []
+        for _date in dates:
+            kickoffs.append({
+                'kickoff_date': _date,
+                'sites': items.get(_date, [])
+            })
+        return kickoffs
+
+
     print('query:', query)
-    return Response([{'id': _id, 'name': _name} for _id, _name in Region.choices() if _id != 'R0'])
+    filtering = {}
+    filtering.update(**parse_date())
+    filtering.update(**parse_time())
+    filtering.update(**parse_region())
+    filtering.update(**parse_course())
+    filtering.update(**parse_price())
+    print('filtering::: {}'.format('-'*20))
+    pprint(filtering)
+
+    dates = sorted(filtering.get('kickoff_date__in', []))
+    bookings = defaultdict(list)
+    for _booking in Booking.objects.filter(**filtering):
+        bookings[(_booking.golf_course.name, _booking.golf_course.id)].append(_booking)
+
+    courses = []
+    for (_name, _id) in sorted(bookings.keys()):
+        courses.append({
+            'id': _id,
+            'name': _name,
+            'kickoffs': get_kickoffs(dates, bookings[(_name, _id)])
+        })
+
+    return Response({
+        'kickoff_dates': [{'date': _d, 'weekday': convert_str_to_weekday(_d)} for _d in dates],
+        'courses': courses,
+    })
