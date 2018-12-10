4a381bf622be614f0d97ee98c7d455db6e5c3906
dongchimi <dongchimi@gmail.com>
2018-12-10 14:21:48 +0900

    모든 시간대가 출력 안되는 오류 수정

--- client/src/courses/components/BookingCell.js
+++ client/src/courses/components/BookingCell.js
@@ -14,25 +14,31 @@ class BookingCell extends React.Component {
 
         const kickoff = CollectionUtility.groupBy(this.props.kickoff.sites, site => site.hour);
 
+        // console.log(kickoff);
         this.state = {
             kickoff : kickoff
         }
     }
 
     render() {
+        let spans = [];
+        let idx = 0;
+        this.state.kickoff.forEach(_d => {
+            _d.forEach(site => {
+                spans.push((<span  key={`site-${++idx}`} style={{paddingRight: 1 + 'px'}} >
+                     <span className='booking-cell-time badge badge-warning'>
+                         {DateUtility.convert(site.kickoff_time, 'YYYY.MM.DD HH:mm', 'HH:mm')}
+                         <a href={site.booking_url} className="badge badge-primary" target='_blank'>{site.name}</a>
+                     </span>
+                 </span>));
+            });
+        });
+
+
         return (
             <td className={'booking-cell'}>
-            {!this.state.kickoff.values().next().done ?
-                this.state.kickoff.values().next().value.map((site, idx) => {
-                    return (
-                        <span key={`site-${idx}`} style={{paddingRight: 1 + 'px'}} >
-                            <span className='booking-cell-time badge badge-warning'>
-                                {DateUtility.convert(site.kickoff_time, 'YYYY.MM.DD HH:mm', 'HH:mm')}
-                                <a href={site.booking_url} className="badge badge-primary" target='_blank'>{site.name}</a>
-                            </span>
-                        </span> );
-                })
-                : ''
+            {
+                spans
             }
             </td>
         )
