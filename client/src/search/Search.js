ca959fe34352f576cc065e6272fc334081fec07b
shine <shine@wivlabs.com>
2018-12-31 14:01:29 +0900

    변경된 디자인 시안에 따라 스타일 변경. 검색영역 gradation, contact us 추가, logo 추가

--- client/src/search/Search.js
+++ client/src/search/Search.js
@@ -35,45 +35,48 @@ class Search extends React.Component {
     render() {
         return (
             <div>
-                <header className="masthead d-flex">
-                    <div className="container my-auto">
-                        <div className="row">
-                            <div className="col-lg-12 mx-auto">
-                                <div className="border rounded border-light float-right bg-search-panel p-5">
-                                    <form>
-                                        <div className="mb-5">
-                                            <MultiDateForm bookingdates={this.props.booking_dates} hastitle="false"
-                                                           onChange={(_bookingDates) => this.state.booking_dates = _bookingDates.map(_d => moment(_d).format('YYYY-MM-DD'))}/>
-                                        </div>
-                                        <div className="mb-5">
-                                            <TimeRangeForm timerange={this.props.time_range} hastitle="false"
-                                                           onChange={(_timeRange) => this.state.time_range = _timeRange}/>
-                                        </div>
-                                        <div className="mb-5">
-                                            <GreenFeeRangeForm
-                                                greenfeerange={this.props.greenfee_range} hastitle="false"
-                                                onChange={(_range) => this.state.greenfee_range = _range}/>
-                                        </div>
-                                        <div className="mb-5">
-                                            <RegionForm region={this.props.region} hastitle="false"
-                                                        onChange={(_region) => this.setState({region: _region})}/>
-                                        </div>
-                                        <div className="mb-5">
-                                            <CourseForm course={this.props.course}
-                                                        region={this.state.region} hastitle="false"
-                                                        onChange={(_course) => this.state.course = _course}/>
-                                        </div>
-                                        <div className="float-right">
-                                            <button type="button" className="btn btn-danger" onClick={this.clickSearch}>
-                                                <i className="fa fa-golf-ball"></i> 검색
-                                            </button>
-                                        </div>
-                                    </form>
+                <header>
+                    <a href="/"><img src="/assets/image/logo.png" className="logo"/></a>
+                </header>
+                <section className="masthead">
+                    <div className="search-panel-wrapper">
+                        <div className="float-right bg-search-panel p-5 mr-3">
+                            <form>
+                                <div className="mb-5">
+                                    <MultiDateForm bookingdates={this.props.booking_dates} hastitle="false"
+                                                   onChange={(_bookingDates) => this.state.booking_dates = _bookingDates.map(_d => moment(_d).format('YYYY-MM-DD'))}/>
                                 </div>
-                            </div>
+                                <div className="mb-5">
+                                    <TimeRangeForm timerange={this.props.time_range} hastitle="false"
+                                                   onChange={(_timeRange) => this.state.time_range = _timeRange}/>
+                                </div>
+                                <div className="mb-5">
+                                    <GreenFeeRangeForm
+                                        greenfeerange={this.props.greenfee_range} hastitle="false"
+                                        onChange={(_range) => this.state.greenfee_range = _range}/>
+                                </div>
+                                <div className="mb-5">
+                                    <RegionForm region={this.props.region} hastitle="false"
+                                                onChange={(_region) => this.setState({region: _region})}/>
+                                </div>
+                                <div className="mb-5">
+                                    <CourseForm course={this.props.course}
+                                                region={this.state.region} hastitle="false"
+                                                onChange={(_course) => this.state.course = _course}/>
+                                </div>
+                                <div className="float-right">
+                                    <button type="button" className="btn btn-danger" onClick={this.clickSearch}>
+                                        <i className="fa fa-golf-ball"></i> 검색
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </header>
+                </section>
+                <footer>
+                    <a href="mailto:info@wivlabs.com?subject=Golf1Shot%20문의"
+                       className="customer-service text-dark font-weight-bold h4 mr-4 mb-4">Contact Us</a>
+                </footer>
             </div>
         )
     }
