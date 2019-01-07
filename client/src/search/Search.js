9cbfd9b9dde07d30924affeab46f8a201e26fb5d
shine <shine@wivlabs.com>
2019-01-07 11:07:47 +0900

    달라진 첫 페이지 시안에 따라 디자인 변경

--- client/src/search/Search.js
+++ client/src/search/Search.js
@@ -36,57 +36,50 @@ class Search extends React.Component {
         return (
             <div>
                 <header>
-                    <a href="/"><img src="/assets/image/logo.png" className="logo"/></a>
-                    <span className="service-title h1">
-                        세상의 모든 골프 부킹을 <br/>
-                        한 눈에 확인하세요
-                    </span>
+                    {/*<a href="/"><img src="/assets/image/logo.png" className="logo"/></a>*/}
                 </header>
+                <div className="service-title h1 text-center text-light">
+                    세상의 모든 골프 부킹을 <br/>
+                    한 눈에 확인하세요
+                </div>
                 <section className="masthead">
-                    <div className="ad-section">
-                        Ad Area
-                    </div>
-                    <div className="search-panel-wrapper">
-                        <div className="float-right bg-search-panel p-5 mr-3">
-                            <form>
-                                <div className="mb-5">
-                                    <MultiDateForm bookingdates={this.props.booking_dates} hastitle="false"
-                                                   onChange={(_bookingDates) => this.state.booking_dates = _bookingDates.map(_d => moment(_d).format('YYYY-MM-DD'))}/>
-                                </div>
-                                <div className="mb-5">
-                                    <TimeRangeForm timerange={this.props.time_range} hastitle="false"
-                                                   onChange={(_timeRange) => this.state.time_range = _timeRange}/>
-                                </div>
-                                <div className="mb-5">
-                                    <GreenFeeRangeForm
-                                        greenfeerange={this.props.greenfee_range} hastitle="false"
-                                        onChange={(_range) => this.state.greenfee_range = _range}/>
-                                </div>
-                                <div className="mb-5">
-                                    <RegionForm region={this.props.region} hastitle="false"
-                                                onChange={(_region) => this.setState({region: _region})}/>
-                                </div>
-                                <div className="mb-5">
-                                    <CourseForm course={this.props.course}
-                                                region={this.state.region} hastitle="false"
-                                                onChange={(_course) => this.state.course = _course}/>
-                                </div>
-                                <div className="float-right">
-                                    <button type="button" className="btn btn-danger" onClick={this.clickSearch}>
-                                        <i className="fa fa-golf-ball"></i> 검색
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
+                    <div className="float-right bg-search-panel p-5 mr-3 border border-light">
+                        <form>
+                            <div className="mb-5">
+                                <MultiDateForm bookingdates={this.props.booking_dates} hastitle="false"
+                                               onChange={(_bookingDates) => this.state.booking_dates = _bookingDates.map(_d => moment(_d).format('YYYY-MM-DD'))}/>
+                            </div>
+                            <div className="mb-5">
+                                <TimeRangeForm timerange={this.props.time_range} hastitle="false"
+                                               onChange={(_timeRange) => this.state.time_range = _timeRange}/>
+                            </div>
+                            <div className="mb-5">
+                                <GreenFeeRangeForm
+                                    greenfeerange={this.props.greenfee_range} hastitle="false"
+                                    onChange={(_range) => this.state.greenfee_range = _range}/>
+                            </div>
+                            <div className="mb-5">
+                                <RegionForm region={this.props.region} hastitle="false"
+                                            onChange={(_region) => this.setState({region: _region})}/>
+                            </div>
+                            <div className="mb-5">
+                                <CourseForm course={this.props.course}
+                                            region={this.state.region} hastitle="false"
+                                            onChange={(_course) => this.state.course = _course}/>
+                            </div>
+                            <div className="float-right">
+                                <button type="button" className="btn btn-danger" onClick={this.clickSearch}>
+                                    <i className="fa fa-golf-ball"></i> 검색
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </section>
                 <footer>
-                    <a href="mailto:info@wivlabs.com?subject=Golf1Shot%20문의"
-                       className="customer-service text-dark font-weight-bold h4 mr-4 mb-4">Contact Us</a>
-                    <span className="golf-channels text-dark font-weight-bold h2 mb-4">
-                        <label className="h3">현재 서비스 중인 채널</label><br/>
-                        SBS골프, X골프, JTBC골프
-                    </span>
+                    <div className="contact-us text-light">
+                        <div className="text-center">현재 서비스 중인 채널 : SBS골프, X골프, JTBC골프</div>
+                        <div className="text-center">부킹 채널 추가 및 광고 문의는 <a href="mailto:info@wivlabs.com?subject=Golf1Shot%20문의">contact us</a> 혹은 <ins>info@wivlabs.com</ins> 으로 메일 주시면 검토 후 회신 드리겠습니다.</div>
+                    </div>
                 </footer>
             </div>
         )
